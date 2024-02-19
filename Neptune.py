e4d8d447921e0000aac4d853efc3ed4674d17f18
LIFECRZY <cozy.bul@gmail.com>
2024-02-19 01:52:15 -0500

    update_email_queue update

--- Neptune.py
+++ Neptune.py
@@ -8,6 +8,7 @@ from email_service import process_email_queue
 from contacts import contacts
 import threading
 import random
+from file_monitor import BeatManager
 
 load_dotenv()  # This loads the .env file at the root of the project
 
@@ -36,11 +37,12 @@ def update_email_queue(email_queue, new_beats, contacts):
             email_queue.put({'contact': contact, 'beat': beat})
 
 def scheduled_beat_check(directory, interval, email_queue, contacts):
-    new_beats = scan_and_queue_existing_mp3s(directory)
-    update_email_queue(email_queue, new_beats, contacts)
-
-    # Schedule the next execution of this function 
-    threading.Timer(interval, scheduled_beat_check, [directory, interval, email_queue, contacts]).start()
+    beat_manager = BeatManager()
+    new_beats = beat_manager.scan_and_queue_existing_mp3s(directory)
+    beat_manager.update_email_queue_with_new_beats(email_queue, new_beats, contacts)
+    
+    # Reschedule the beat check
+    threading.Timer(interval, scheduled_beat_check, args=(directory, interval, email_queue, contacts)).start()
 
 def populate_email_queue(beats, contacts):
     for beat in beats:
@@ -52,18 +54,25 @@ def ensure_log_directory_exists():
     if not os.path.exists(Config.LOG_DIR):
         os.makedirs(Config.LOG_DIR)
 
-ensure_log_directory_exists()
+
 
 if __name__ == '__main__':
+    # Initial setup: Ensure log directory exists and populate the email queue
+    ensure_log_directory_exists()
     valid_beats = scan_and_queue_existing_mp3s(Config.MONITOR_DIRECTORY)
     populate_email_queue(valid_beats, contacts)
-
+    
+    # Start the email processing thread
     email_thread = threading.Thread(target=process_email_queue, args=(email_queue,))
     email_thread.start()
 
+
+    # Start the filesystem observer thread
     observer_thread = threading.Thread(target=start_observer) # Start the observer in a new thread
     observer_thread.start()
 
+    # Start the scheduled beat check
     scheduled_beat_check(Config.MONITOR_DIRECTORY, 25200, email_queue, contacts)  # 25200 seconds = 7 hours
 
+    # Start the Flask application
     app.run(debug=True, port=Config.FLASK_RUN_PORT, use_reloader=False)
